190a9e9c222100eabaa9a6d26614615cde7f914c
mauriani <maurianim@gmail.com>
2022-01-25 17:04:57 -0300

    attrs

--- android/app/src/main/java/com/rentx/MainActivity.java
+++ android/app/src/main/java/com/rentx/MainActivity.java
@@ -7,8 +7,7 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
-import expo.modules.splashscreen.singletons.SplashScreen;
-import expo.modules.splashscreen.SplashScreenImageResizeMode;
+import expo.modules.ReactActivityDelegateWrapper;
 
 public class MainActivity extends ReactActivity {
   @Override
@@ -31,11 +30,14 @@ public class MainActivity extends ReactActivity {
 
     @Override
     protected ReactActivityDelegate createReactActivityDelegate() {
-        return new ReactActivityDelegate(this, getMainComponentName()) {
-            @Override
-            protected ReactRootView createRootView() {
-                return new RNGestureHandlerEnabledRootView(MainActivity.this);
-            }
-        };
+        return new ReactActivityDelegateWrapper(
+        this,
+       new ReactActivityDelegate(this, getMainComponentName()) {
+        @Override
+       protected ReactRootView createRootView() {
+          return new RNGestureHandlerEnabledRootView(MainActivity.this);
+       }
+      }
+     )
     }
 }
